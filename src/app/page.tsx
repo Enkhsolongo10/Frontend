7162862d7b71e2001e99e768cf28c7ecbaf3cfbe
24LP1557 24LP1557 <24LP1557@G305-CC0109.local>
2025-01-08 18:08:37 +0800

    frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
-
+'use client';
+import { useEffect, useState } from "react";
 export default function Home() {
+const[moviesa,setMovies]=useState<movies[]>([])
+  async function getMovies(){
+    const res = await fetch("http://localhost:4000/movies");
+    const data = await res.json();
+    setMovies(data)
+    console.log(data)
+  };
+
+  useEffect(() => {
+    getMovies(); 
+  }, [moviesa]);
+  type movies={
+    id:number,
+    name:string
+  }
   return (
-   <div></div>
+   <div>{moviesa.map((kanu)=>(
+    <div key={kanu.id}>{kanu.name}</div>
+   ))}</div>
   );
-}
+};  
